b759cc33bc4b86c4cceb35a9df237c0a6448ba7c
Pierluca <164493911+piercode7@users.noreply.github.com>
2024-10-28 23:46:02 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,12 +6,15 @@ if [ ! -d "javafx-sdk-21.0.2/lib" ]; then
   exit 1
 fi
 
-# Esegui il comando per avviare l'applicazione
+# Compila il progetto con Maven per assicurarsi che tutte le dipendenze siano aggiornate
+mvn clean compile -Dmdep.outputFile=classpath.txt
+
+# Esegui il comando per avviare l'applicazione con tutte le dipendenze del classpath
 java --module-path "${PWD}/javafx-sdk-21.0.2/lib" \
      --add-modules javafx.controls,javafx.fxml,javafx.media \
      --add-exports=javafx.graphics/com.sun.glass.utils=ALL-UNNAMED \
      --add-exports=javafx.media/com.sun.media.jfxmediaimpl=ALL-UNNAMED \
      --add-exports=javafx.base/com.sun.javafx=ALL-UNNAMED \
      --add-exports=javafx.graphics/com.sun.javafx.tk=ALL-UNNAMED \
-     -cp "target/classes:$(mvn dependency:build-classpath -Dmdep.outputFile=classpath.txt && cat classpath.txt)" \
+     -cp "target/classes:$(cat classpath.txt)" \
      org.mypulse.view.MainView
