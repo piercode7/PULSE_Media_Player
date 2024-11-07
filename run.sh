a4511aafccc15da8eba820c4555ce55005db9c93
piercode7 <pierlucaped@gmail.com>
2024-11-07 03:32:01 +0100

    Aggiornamento pom e run da Ubuntu

--- run.sh
+++ run.sh
@@ -6,15 +6,14 @@ if [ ! -d "javafx-sdk-21.0.2/lib" ]; then
   exit 1
 fi
 
-# Compila il progetto con Maven per assicurarsi che tutte le dipendenze siano aggiornate
-mvn clean compile -Dmdep.outputFile=classpath.txt
+mvn clean compile dependency:build-classpath
 
-# Esegui il comando per avviare l'applicazione con tutte le dipendenze del classpath
 java --module-path "${PWD}/javafx-sdk-21.0.2/lib" \
      --add-modules javafx.controls,javafx.fxml,javafx.media \
      --add-exports=javafx.graphics/com.sun.glass.utils=ALL-UNNAMED \
      --add-exports=javafx.media/com.sun.media.jfxmediaimpl=ALL-UNNAMED \
      --add-exports=javafx.base/com.sun.javafx=ALL-UNNAMED \
      --add-exports=javafx.graphics/com.sun.javafx.tk=ALL-UNNAMED \
-     -cp "target/classes:$(cat classpath.txt)" \
+     -cp "target/classes:$(cat target/classpath.txt)" \
      org.mypulse.view.MainView
+
